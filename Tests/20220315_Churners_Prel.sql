ed006bd262404f13795ff3b276a433e4832f60cc
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-03-15 18:57:07 -0500

    Create 20220315_Churners_Prel.sql

---
+++ Tests/20220315_Churners_Prel.sql
@@ -0,0 +1,41 @@
+WITH 
+
+CustomersBegFebruary AS (
+SELECT DISTINCT act_acct_cd, min(act_acct_inst_dt) AS MinStartDate,DATE_TRUNC(load_dt, MONTH) AS February
+FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+WHERE org_cntry="Jamaica" AND load_dt='2022-02-01'
+AND (fi_outst_age <90 OR fi_outst_age is null)
+GROUP BY act_acct_cd,February
+)
+, CustomersBegFebruaryCount AS (
+SELECT DISTINCT COUNT(act_acct_cd) AS InitialAccounts,February
+FROM CustomersBegFebruary 
+WHERE MinStartDate<'2022-02-01'
+GROUP BY February
+)
+, CustomersEndFebruary AS (
+SELECT COUNT(DISTINCT act_acct_cd) AS EndingAccounts,DATE_TRUNC(load_dt, MONTH) AS February
+FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+WHERE org_cntry="Jamaica" AND load_dt='2022-02-28'
+ AND (fi_outst_age <90 OR fi_outst_age is null)
+GROUP BY February
+)
+, GrossAddsFebruary AS (
+SELECT DISTINCT act_acct_cd, min(act_acct_inst_dt) AS MinStartDate,DATE_TRUNC(load_dt, MONTH) AS February
+FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+WHERE org_cntry="Jamaica" AND load_dt='2022-02-28'
+ AND (fi_outst_age <90 OR fi_outst_age is null)
+GROUP BY act_acct_cd, February
+HAVING MinStartDate>='2022-02-01'
+)
+, GrossAddsFebruaryCount AS(
+SELECT COUNT(DISTINCT act_acct_cd) AS GrossAdds, February
+FROM GrossAddsFebruary
+GROUP BY February
+)
+
+SELECT InitialAccounts, EndingAccounts, GrossAdds, (InitialAccounts+GrossAdds-EndingAccounts) AS Churners
+FROM CustomersBegFebruaryCount b 
+ INNER JOIN CustomersEndFebruary e ON b.February=e.February
+ INNER JOIN GrossAddsFebruaryCount g ON b.February=g.February
+
