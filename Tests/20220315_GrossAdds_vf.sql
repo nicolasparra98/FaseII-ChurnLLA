71b7322e4de34acedf68a2e798534fdfee595f44
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-03-15 16:07:47 -0500

    Update 20220315_GrossAdds_vf.sql

--- Tests/20220315_GrossAdds_vf.sql
+++ Tests/20220315_GrossAdds_vf.sql
@@ -1,13 +1,13 @@
-WITH 
+WITH
 
-FechaInicioFebrero AS (
+GrossAddsFebruary AS (
 SELECT DISTINCT act_acct_cd, min(act_cust_strt_dt) AS MinStartDate
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
-WHERE org_cntry="Jamaica" --AND load_dt='2022-02-28'
+WHERE org_cntry="Jamaica" AND load_dt='2022-02-28'
  AND (fi_outst_age <90 OR fi_outst_age is null)
 GROUP BY act_acct_cd
 HAVING MinStartDate>='2022-02-01'
 )
 
-SELECT COUNT(DISTINCT ACT_ACCT_CD) AS GrossAddsFeb
-FROM FechaInicioFebrero
+SELECT COUNT(DISTINCT act_acct_cd) AS GrossAddsFeb
+FROM GrossAddsFebruary 
