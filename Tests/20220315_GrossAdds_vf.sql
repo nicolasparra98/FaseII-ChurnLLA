74a1d94449479a4f65126e3885fdecac097ff417
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-03-15 19:06:13 -0500

    Update 20220315_GrossAdds_vf.sql

--- Tests/20220315_GrossAdds_vf.sql
+++ Tests/20220315_GrossAdds_vf.sql
@@ -3,6 +3,7 @@ WITH
 GrossAddsFebruary AS (
 SELECT DISTINCT act_acct_cd, min(act_cust_strt_dt) AS MinStartDate
 FROM `gcp-bia-tmps-vtr-dev-01.gcp_temp_cr_dev_01.cwc_jam_dna_fullmonth_202202` 
+--load_dt: los que están activos el 28 de febrero
 WHERE org_cntry="Jamaica" AND load_dt='2022-02-28'
  AND (fi_outst_age <90 OR fi_outst_age is null)
 GROUP BY act_acct_cd
