2420ae6ebd85e5c0fe726c55ea6b286a6b445e1c
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-03-25 15:00:48 -0500

    Update 10. CustomerClosingBase.sql

--- PostPaid/CWC/KPIs/10. CustomerClosingBase.sql
+++ PostPaid/CWC/KPIs/10. CustomerClosingBase.sql
@@ -2,9 +2,8 @@ WITH
 MobileClosingBase AS(
 SELECT DISTINCT account_id,DATE_TRUNC(dt,Month) AS Month
 FROM `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.cwc_info_dna_postpaid_history_v2` 
-WHERE org_id = "338" AND dt=LAST_DAY(dt,Month) 
- AND (DATE_DIFF(safe_cast(dt as date),safe_cast(lst_pymt_dt as date),DAY)<=90 OR (dt is null OR lst_pymt_dt is null))
- AND account_type ="Residential"
+WHERE org_id = "338" AND dt=LAST_DAY(dt,Month) AND account_type ="Residential"
+ AND account_status NOT IN('Ceased','Closed','Recommended for cease')
 )
 SELECT Month, COUNT(DISTINCT account_id) AS Records
 FROM MobileClosingBase 
