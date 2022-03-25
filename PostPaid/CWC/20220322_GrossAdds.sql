0a3c01078c8ba00b13f62dac95e11ae78307eb53
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-03-25 17:02:58 -0500

    Update 20220322_GrossAdds.sql

--- PostPaid/CWC/20220322_GrossAdds.sql
+++ PostPaid/CWC/20220322_GrossAdds.sql
@@ -1,13 +1,10 @@
 WITH 
 GrossAddsPostpaid AS (
-SELECT DISTINCT account_id, min(safe_cast(customer_creation_date as date)) AS MinStartDate,DATE_TRUNC(safe_cast(dt as date),MONTH) AS Month
+SELECT DISTINCT account_id, min(safe_cast(account_creation_date as date)) AS MinStartDate
+,DATE_TRUNC(safe_cast(dt as date),MONTH) AS Month
 FROM `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.cwc_info_dna_postpaid_history`   
-WHERE country_name="nan"
- AND (region_name LIKE "%jama%" OR region_name LIKE "%JAMA%" OR region_name LIKE "%Jama%" OR region_name LIKE "%Jami%" 
- OR region_name LIKE "%aica%" OR region_name LIKE "%KINGSTON%"OR region_name LIKE "%Kingston%"OR region_name LIKE "%kingston%")
- AND (DATE_DIFF(safe_cast(dt as date),safe_cast(lst_pymt_dt as date),DAY)<=90 OR (dt is null OR lst_pymt_dt is null))
- AND account_status IN('Working','In Default','Promise to pay','TOSd')
- AND account_type ="Residential"
+WHERE org_id = "338" AND account_type ="Residential"
+ AND account_status NOT IN('Ceased','Closed','Recommended for cease')
 GROUP BY account_id,Month
 HAVING MinStartDate>=Month
 )
