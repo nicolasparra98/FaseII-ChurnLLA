064073919c6591231d7d6a315e42923fdf8903f1
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-03-30 15:14:04 -0500

    Update 20220330_JoinConvergentBase.sql

--- CWP/20220330_JoinConvergentBase.sql
+++ CWP/20220330_JoinConvergentBase.sql
@@ -4,7 +4,7 @@ SELECT DISTINCT *,PARSE_DATE("%Y%m%d",Date) as Mes
 FROM `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.20220330_cwc_base_convergente_enero_febrero` 
 )
 ,DNA AS(
-SELECT DISTINCT DATE_TRUNC(DT,Month) AS Month,ACT_ACCT_CD
+SELECT DISTINCT DATE_TRUNC(DT,Month) AS Month,ACT_ACCT_CD,ACT_CONTACT_PHONE_3 AS CONTACTO
 FROM `gcp-bia-tmps-vtr-dev-01.lla_temp_dna_tables.cwp_info_dna_fixed_history_v2` 
 )
-SELECT DISTINCT * FROM Convergente c INNER JOIN DNA d ON c.SERVICE_id=d.act_acct_cd AND c.Mes=d.Month
+SELECT DISTINCT * FROM Convergente c INNER JOIN DNA d ON SAFE_CAST(c.HOUSEHOLD_id AS STRING)=d.CONTACTO AND c.Mes=d.Month
