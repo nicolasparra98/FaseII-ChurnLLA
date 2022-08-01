0f40b9389d1cc5307778b9943e238bf1204dcfcb
laurahoyos99 <97916353+laurahoyos99@users.noreply.github.com>
2022-08-01 14:43:25 -0500

    Update 00. Grouped_FMC_Table.sql

--- MasterTableAdj/00. Grouped_FMC_Table.sql
+++ MasterTableAdj/00. Grouped_FMC_Table.sql
@@ -9,7 +9,7 @@ SELECT distinct month as Month,final_bom_activeflag as Final_BOM_ActiveFlag,fina
 ,count(distinct e_mobile_id) as E_Mobile_ID,e_mobilergus as E_MobileRGUs,e_mobiletenure as E_MobileTenure,mobilemainmovement as MobileMainMovement,mobilespinflag as MobileSpinFlag,FmcFlagMob,drc	as DRC,mobilechurnflag as MobileChurnFlag,mobilechurnertype as MobileChurnerType
 ,finalchurnflag as FinalChurnFlag,churntypefinalflag as ChurnTypeFinalFlag,b_final_tenure as B_Final_Tenure,e_final_tenure as E_Final_Tenure,b_final_techflag as B_Final_TechFlag,e_final_techflag as E_Final_TechFlag,b_fmctype as B_FMCType,e_fmctype as E_FMCType,b_fmcsegment as B_FMCSegment,e_fmcsegment as E_FMCSegment,rejoinerflag as RejoinerFlag,rejoinerfmcflag as RejoinerFMCFlag,waterfall_flag as Waterfall_Flag,Downsell_Split,Downspin_Split
 ,fixedmonth
-,fixedchurnsubtype,fixed_prmonth,fixed_rejoinermonth,gap,mobile_month,mobile_prmonth,mobile_rejoinermonth,b_totalrgus,e_totalrgus,potentialrejoinerflag
+,fixedchurnsubtype,fixed_prmonth,fixed_rejoinermonth,gap,mobile_month,mobile_prmonth,mobile_rejoinermonth,b_totalrgus,e_totalrgus,potentialrejoinerflag,Partial_Total_ChurnFlag
 ,count(distinct finalaccount) as FinalAccount,count(distinct fixedaccount) as FixedAccount,count(distinct mobile_account) as Mobile_Account,avg(cast(round(mobile_mrc_diff,0) as int)) as Mobile_MRC_Diff,sum(cast(round(b_avgmobilemrc,0) as int)) as B_AvgMobileMRC,sum(cast(round(b_mobilemrc,0) as int)) as B_MobileMRC,sum(cast(round(b_total_mrc,0) as int)) as B_Total_MRC,sum(cast(round(e_total_mrc,0) as int)) as E_Total_MRC,sum(cast( round(e_avgmobilemrc,0) as int))	as E_AvgMobileMRC,sum(cast( round(e_mobilemrc,0) as int)) as E_MobileMRC
 ,count(distinct household_id) as Household_id,count(distinct mobile_household_id) as Mobile_Household_id
 ,count(distinct fixedaccount_bom) as fixedaccount_bom,count(distinct b_bb) as b_bb,count(distinct b_tv) as b_tv,count(distinct b_vo) as b_vo,count(distinct e_bb) as e_bb,count(distinct e_tv) as e_tv,count(distinct e_vo) as e_vo
@@ -22,4 +22,4 @@ group by 1,2,3,4,5,6,7 --,8
 ,49,50--,51
 ,52,53,54--,55
 ,56,57--,58
-,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92
+,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93
